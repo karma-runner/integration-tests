80daaab40d6ac433bfebd55534a756f5b6889cc8
Vojta Jina <vojta.jina@gmail.com>
2014-03-31 11:24:40 -0700

    Support filtering with -g (grep).

--- run.sh
+++ run.sh
@@ -1,17 +1,46 @@
-LOCAL_PKG_TO_OVERRIDE=$1
+#!/bin/sh
+#
+# Run the integration tests.
+# Each subdirectory that has a package.json is considered to be an integration test.
+#
+# Arguments:
+#   -g <expression> Run only tests matching this expression.
+#                   The expression is passed to egrep -e <expression>.
+#
+# Example:
+# ./run.sh -g jasmine karma-0.12.1.tgz
+#
+# -> Run jasmine and jasmine_2 tests using Karma from karma-0.12.1.tgz package.
 
-set -e
 
-for DIR in ./*; do
-  if [ -f "$DIR/package.json" ]; then
+
+# Parse arguments.
+GREP=".*"
+
+while getopts "g:" OPTION; do
+  case "$OPTION" in
+    g)
+    GREP="$OPTARG"
+    ;;
+  esac
+done
+
+PKG_TO_OVERRIDE=${@:$OPTIND:1}
+
+
+# Run all the matching tests.
+for DIR in *; do
+  MATCH_COUNT=$(echo $DIR | egrep -c -e "$GREP")
+
+  if [ -f "$DIR/package.json" -a $MATCH_COUNT -gt 0 ]; then
     echo "=============================================================="
-    echo " ${DIR} with ${LOCAL_PKG_TO_OVERRIDE}"
+    echo " ${DIR} with ${PKG_TO_OVERRIDE}"
     echo "=============================================================="
 
-    cd $DIR
-    npm install > /dev/null
-    npm install ${LOCAL_PKG_TO_OVERRIDE} > /dev/null
-    npm test
+    cd ./$DIR
+    npm install > /dev/null || exit 1
+    npm install ${PKG_TO_OVERRIDE} > /dev/null || exit 1
+    npm test || exit 1
     cd ..
   fi
 done
